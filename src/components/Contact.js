af0ad80d2fa4c10e99921df489c7a79b65df3d25
azadtamal <mrazadt@gmail.com>
2021-09-10 19:44:55 +0600

    react app

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import "./Contact.css";
 
 class Contact extends Component {
   render() {
     const { name, gmail, phone } = this.props;
     return (
       <div>
-        <h2>name : {name}</h2>
+        <h1>name : {name}</h1>
         <p>gmail: {gmail}</p>
         <p>phone : {phone}</p>
       </div>
