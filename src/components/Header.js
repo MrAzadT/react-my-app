e0e1f5d91b9540202133e50158714a9f145875ba
azadtamal <mrazadt@gmail.com>
2021-09-10 19:41:11 +0600

    react app

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Header = (props) => {
   const { property } = props;
-  return <h1 style={{ color: "red" }}>{property}</h1>;
+  return <h1 style={head}>{property}</h1>;
 };
 
+const head = {
+  color: "green",
+  fontSize: "100px",
+};
 export default Header;
