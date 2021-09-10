f954f752a2764d98d7adf8af6675a9c61d9486c5
azadtamal <mrazadt@gmail.com>
2021-09-10 21:01:26 +0600

    react app

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ function App() {
   return (
     <div className="App">
       <Header property="abid azad" />
-      <Contact name="azad" gmail="mrazadt@gmail.com" phone="222-222-222" />
-      <Contact name="rakon" gmail="rakib@gmail.com" phone="000-000 -222" />
-      <Contact name="anha" gmail="anha@gmail.com" phone="333-555-222" />
+
+      <div className="container">
+        <Contact name="azad" gmail="mrazadt@gmail.com" phone="222-222-222" />
+        <Contact name="rakon" gmail="rakib@gmail.com" phone="000-000 -222" />
+        <Contact name="anha" gmail="anha@gmail.com" phone="333-555-222" />
+      </div>
     </div>
   );
 }
