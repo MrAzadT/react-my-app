590048fe667a245eb4740be0ccc1fda5ebe71e0d
azadtamal <mrazadt@gmail.com>
2021-09-11 20:36:23 +0600

    react app

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 
 class Contact extends Component {
   render() {
-    const { name, gmail, phone } = this.props;
+    const { name, gmail, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h1 className="mx-4">Name : {name}</h1>
+        <h1 className="mx-4">
+          {name}
+          <i onClick={this.onShowClick} class="fas fa-sort-down"></i>
+        </h1>
         <ul className="list-item">
-          <li className="list-group-item">Gmail : {gmail} </li>
-          <li className="list-group-item">Phone : {phone}</li>
+          <li className="list-group-item">{gmail} </li>
+          <li className="list-group-item">{phone}</li>
         </ul>
       </div>
     );
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,48 +1,41 @@
 import React, { Component } from "react";
-
+import Contact from "./Contact";
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "kamal",
-          gmail: "kamal@gmail.com",
-          phone: "344-554-4543",
-        },
-        {
-          id: 2,
-          name: "tamal",
-          gmail: "tamal@gmail.com",
-          phone: "344-554-4543",
-        },
-        {
-          id: 3,
-          name: "anha",
-          gmail: "anha@gmail.com",
-          phone: "344-554-4543",
-        },
-        {
-          id: 4,
-          name: "lima",
-          gmail: "lima@gmail.com",
-          phone: "344-554-4543",
-        },
-      ],
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "kamal",
+        gmail: "kamal@gmail.com",
+        phone: "344-554-4543",
+      },
+      {
+        id: 2,
+        name: "tamal",
+        gmail: "tamal@gmail.com",
+        phone: "344-554-4543",
+      },
+      {
+        id: 3,
+        name: "anha",
+        gmail: "anha@gmail.com",
+        phone: "344-554-4543",
+      },
+      {
+        id: 4,
+        name: "lima",
+        gmail: "lima@gmail.com",
+        phone: "344-554-4543",
+      },
+    ],
+  };
 
   render() {
     const { contacts } = this.state;
     return (
       <div>
         {contacts.map((contact) => (
-          <div>
-            <h1>{contact.name}</h1>
-            <p>{contact.gmail}</p>
-            <p>{contact.phone}</p>
-          </div>
+          <Contact key={contact.id} contact={contact} />
         ))}
       </div>
     );
